3778711ba426ab3c2bfa63ed0dee7ed998e9b58f
avideb24 <debavi24@gmail.com>
2024-05-25 11:21:46 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 4000;
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://bazar-zone:wUdt7b5yNlCPxLFC@cluster0.em2vhup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const productCollection = client.db("Bazar-Zone").collection('products');
+
+
+    // add product
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
@@ -11,5 +49,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("Server is running succcesfully...");
+    console.log("Port:", port);
 })
