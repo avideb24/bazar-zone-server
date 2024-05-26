770c27ef05377d7d7de3ab7a49deb100c9f9404e
avideb24 <debavi24@gmail.com>
2024-05-26 14:08:28 +0600

    product add and get api created

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const cors = require('cors')
 const port = process.env.PORT || 4000;
 require('dotenv').config();
 
 const app = express();
+
+// middlewares
+app.use(cors());
 app.use(express.json());
 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://bazar-zone:wUdt7b5yNlCPxLFC@cluster0.em2vhup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.em2vhup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +32,17 @@ async function run() {
 
 
     // add product
+    app.post('/products', async(req,res) => {
+        const product = req.body;
+        const result = await productCollection.insertOne(product);
+        res.send(result);
+    })
+
+    // get all products
+    app.get('/products', async(req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    })
 
     
 
